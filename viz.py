5858c12ea9565c69338f29eb2076b720bbe98e9a
Simon Pastor <68235075+simonjpastor@users.noreply.github.com>
2022-01-12 19:48:19 +0100

    Update viz.py

--- viz.py
+++ viz.py
@@ -17,7 +17,7 @@ import streamlit as st
 
 
 APP_NAME = "Analyse PQR"
-st.markdown("<h1 style='text-align: center; color: black;'>Analyse PQR</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; color: black;'>Thèmes et Candidats mentionnés dans la Presse Quotidienne Régionale</h1>", unsafe_allow_html=True)
 
 y = pd.read_csv("y.csv", index_col=0)
 x = pd.read_csv("x.csv",index_col=0)
