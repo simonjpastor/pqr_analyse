ec66d6c5b72f41026535f9f39439f3b56a26c98b
Simon Pastor <68235075+simonjpastor@users.noreply.github.com>
2022-01-12 19:47:15 +0100

    Update viz.py

--- viz.py
+++ viz.py
@@ -17,8 +17,7 @@ import streamlit as st
 
 
 APP_NAME = "Analyse PQR"
-
-st.title(APP_NAME)
+st.markdown("<h1 style='text-align: center; color: black;'>Analyse PQR</h1>", unsafe_allow_html=True)
 
 y = pd.read_csv("y.csv", index_col=0)
 x = pd.read_csv("x.csv",index_col=0)
