19e8c402142684f7d34cbaad11f5bb67076555ee
Simon Pastor <68235075+simonjpastor@users.noreply.github.com>
2022-01-13 10:00:56 +0100

    Update general_functions.py

--- general_functions.py
+++ general_functions.py
@@ -10,17 +10,17 @@ import plotly.graph_objs as go
 
 def plot(data):
     fig = go.Figure()
-    # color_dict = {'immigration':"#808B96", 'economie':"#f6b26b","vaccin":"#f6b26b",'securite':"#ffff00", 'gouvernement':"#b4a7d6",
-    #    'candidats':"#6aa84f", 'delinquance':"#e06666", 'environnement_cat_nat':"#6d9eeb", 'environnement':"#AED6F1",
-    #    'europe':"#0000ff", 'pouvoir_dachat':"#cc0000", 'education':"#ff9900", 'violence_aux_femmes':"#6C3483 ",
-    #    'emploi':"#1155cc", 'retraite':"#784212", 'industrie':"#ff00ff", 'mobilite_transport':"#38761d"}
+    color_dict = {'immigration':"#808B96", 'economie':"#f6b26b","vaccin":"#f6b26b",'securite':"#ffff00", 'gouvernement':"#b4a7d6",
+       'candidats':"#6aa84f", 'delinquance':"#e06666", 'environnement_cat_nat':"#6d9eeb", 'environnement':"#AED6F1",
+       'europe':"#0000ff", 'pouvoir_dachat':"#cc0000", 'education':"#ff9900", 'violence_aux_femmes':"#6C3483 ",
+       'emploi':"#1155cc", 'retraite':"#784212", 'industrie':"#ff00ff", 'mobilite_transport':"#38761d"}
     for i in data.columns:
         fig.add_trace(go.Bar(
         y=data[i],
         x=data.index,
         name=f"{i} %",
         marker=dict(
-            #color=color_dict[i],
+            color=color_dict[i],
             line=dict(color='rgba(0,128,0, 0.5)', width=0.05))))
 
     fig.update_layout(
