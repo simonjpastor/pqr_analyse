331f9cd5a2ba9e778513e811c88b2afe23263059
Simon Pastor <68235075+simonjpastor@users.noreply.github.com>
2022-01-13 17:03:36 +0100

    Update general_functions.py

--- general_functions.py
+++ general_functions.py
@@ -101,6 +101,7 @@ def plot_all(data):
             line=dict(color='rgba(0,128,0, 0.5)', width=0.05))))
 
     fig.update_layout(
+        legend_title="Région",
         yaxis=dict(
         title_text="Poids %",
         ticktext=["0%", "20%", "40%", "60%","80%","100%"],
