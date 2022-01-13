6a6d6866226973b0bda325a34143410698e22bad
Simon Pastor <68235075+simonjpastor@users.noreply.github.com>
2022-01-13 18:29:55 +0100

    Update viz.py

--- viz.py
+++ viz.py
@@ -26,7 +26,7 @@ y.insert(0,"",0)
 y = presse_nationale.join(y)
 x = pd.read_csv("y.csv",index_col=0)
 
-candidats_pn = pd.read_csv("pqr_analysis/candidats_presse.csv",index_col=0).transpose().join(x)
+candidats_pn = pd.read_csv("pqr_analysis/candidats_presse.csv",index_col=0)
 
 st.plotly_chart(plot_all(y), use_column_width=True)
 st.plotly_chart(plot_candidates(x), use_column_width=True)
