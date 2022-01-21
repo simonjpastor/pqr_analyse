3a8b0b775ebbcf73db2be28158a100a00498ee27
Simon Pastor <68235075+simonjpastor@users.noreply.github.com>
2022-01-21 09:57:06 +0100

    Update viz.py

--- viz.py
+++ viz.py
@@ -29,6 +29,18 @@ x = pd.read_csv("x3.csv",index_col=0)
 candidats_pn = pd.read_csv("candidats_presse_nationale2.csv",index_col=0)
 
 st.write(plot_all(y), use_column_width=True)
+st.markdown("<h2 style='text-align: center; color: black;'>Période du 7 au 13 Janvier</h2>", unsafe_allow_html=True)
+
+presse_nationale = pd.read_csv("presse_nationale.csv", index_col="Unnamed: 0")
+y = pd.read_csv("y2.csv",index_col="Unnamed: 0").transpose()
+y.insert(0,"",0)
+y = presse_nationale.join(y)
+x = pd.read_csv("x2.csv",index_col=0)
+
+candidats_pn = pd.read_csv("candidats_presse_nationale.csv",index_col=0)
+
+st.write(plot_all(y), use_column_width=True)
+
 st.plotly_chart(plot_candidates(x), use_column_width=True)
 st.plotly_chart(plot_national(candidats_pn), use_column_width=True)
 
